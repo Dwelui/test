58d998ee0a99c2ae4388d9f1b953d7fe37fdac03
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-08-25 19:57:09 +0300

    pass callback

--- include/dwelui/test.h
+++ include/dwelui/test.h
@@ -1,6 +1,8 @@
 #ifndef TEST_H
 #define TEST_H
 
-int test_test();
+typedef void (*TestCallback)();
+
+void test_add(const char *domain, const char *name, TestCallback test);
 
 #endif // TEST_H
--- src/test.c
+++ src/test.c
@@ -1,12 +1,35 @@
-#include <stdio.h>
 #include <dwelui/test.h>
+#include <stdio.h>
+#include <sys/types.h>
 
-int main()
-{
+typedef struct {
+    char         domain[32];
+    char         name[64];
+    TestCallback callback;
+} Test;
+
+typedef struct {
+    Test     *tests;
+    u_int16_t count;
+} TestList;
+
+void first_test() {
+    printf("first_test\n");
+}
+
+int main() {
+    test_add("test", "first_test", first_test);
+
     printf("Hello world!");
-
-    test_test();
 
     return 0;
 }
 
+void test_add(const char *domain, const char *name, TestCallback test) {
+    (void)domain;
+    (void)name;
+
+    test();
+
+    return;
+}
