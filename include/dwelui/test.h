31def3794b3ee310990e5a3564b947f538cdba65
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-08-27 21:12:24 +0300

    disable inline

--- include/dwelui/test.h
+++ include/dwelui/test.h
@@ -22,8 +22,8 @@ typedef struct {
 
 #define test_assert(expr)                                                                          \
     do {                                                                                           \
-        extern TestFailResult *dwelui__test_fail(const char *message, const char *file,            \
-                                                 u_int32_t line, const char *function);            \
+        extern TestFailResult *dwelui__test_fail(const char *, const char *, u_int32_t,            \
+                                                 const char *);                                    \
         if (false == (expr)) {                                                                     \
             return dwelui__test_fail(#expr, __FILE__, __LINE__, __func__);                         \
         }                                                                                          \
@@ -32,7 +32,7 @@ typedef struct {
 #define TEST(fn_name, body)                                                                        \
     static TestFailResult *fn_name(void);                                                          \
                                                                                                    \
-    extern void            dwelui__test_register(const char *name, TestFn fn);                     \
+    extern void            dwelui__test_register(const char *, TestFn);                            \
     static void            register_##fn_name(void) __attribute__((constructor));                  \
     static void            register_##fn_name(void) {                                              \
         dwelui__test_register(#fn_name, fn_name);                                                  \
