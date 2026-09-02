45493de809a8eeac5f0a57b666f49c06417b2dc0
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-09-02 22:07:43 +0300

    remove enum default

--- src/test.c
+++ src/test.c
@@ -227,8 +227,6 @@ const char *test_status_to_cstring(TEST_STATUS status) {
             return "failed";
         case TEST_STATUS_PENDING:
             return "pending";
-        default:
-            abort();
     };
 }
 
